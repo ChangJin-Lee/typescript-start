908823a28c28367594f231241427c9ccb63d55f7
ChangJin-Lee <ckdwls525@gmail.com>
2023-06-24 15:02:49 +0900

    Update READONLY and optional

--- grammar/Objects.js
+++ grammar/Objects.js
@@ -8,4 +8,17 @@ var User = {
 function createUser(_a) {
     var string = _a.name, boolean = _a.isPaid;
 }
-createUser({});
+var newUser = { name: "newCJ", isPaid: false, email: "newCJ@cJ.CJ" };
+createUser(newUser);
+createUser({ name: "CJ", isPaid: false });
+function createCourse() {
+    return { name: "reactjs", price: 999 };
+}
+function typereateUser(u) { }
+var myUser = {
+    _id: "12345",
+    name: "h",
+    email: "h@h.com",
+    isActive: false
+};
+myUser.email = "t@t.com";
--- grammar/Objects.ts
+++ grammar/Objects.ts
@@ -16,5 +16,40 @@ function createCourse():{name: string, price: number}{
     return {name: "reactjs", price: 999}
 }
 
+// -------------------------------------------
+
+type User = {
+    readonly _id: string,
+    name: string,
+    email: string,
+    isActive: boolean,
+    credcardDetails?: number
+}
+
+function typereateUser(u: User){}
+
+let myUser: User = {
+    _id: "12345",
+    name: "h",
+    email: "h@h.com",
+    isActive: false
+}
+
+myUser.email = "t@t.com"
+// myUser._id = "4321"
+
+// -------------------------------------------
+
+type cardNumber = {
+    cardnumber: string
+}
+
+type cardDate = {
+    cardDate: string
+}
+
+type cardDetails = cardNumber & cardDate & {
+    cvv: number
+}
 
 export {}
