4c56a888ac09e03d992ea56005d280ba892c87b7
ChangJin-Lee <ckdwls525@gmail.com>
2023-06-29 12:46:57 +0900

    update enum

---
+++ grammar/Enums.ts
@@ -0,0 +1,19 @@
+// const AISLE = 0
+// const MIDDLE = 1
+// const WINDOW = 2
+
+// if (seat === 0 ){
+
+// }
+
+// const 붙은 것 과 아닌 것의 차이.
+const enum SeatChoice {
+    AISLE = 2,
+    MIDDLE = "MIDDLE",
+    WINDOW = "WINDOW",
+    FOURTH = 0,
+    LAST
+}
+
+const hcSeat = SeatChoice.AISLE
+
