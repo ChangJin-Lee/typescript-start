4720fa8024dfdc9d77c9a9e5d8fff68e05e19b6a
ChangJin-Lee <ckdwls525@gmail.com>
2023-07-11 02:53:32 +0900

    update classes

--- tsProject/src/index.ts
+++ tsProject/src/index.ts
@@ -1,1 +1,15 @@
-console.log("typescript is here");
+// console.log("typescript is here");
+
+class User {
+    email: string
+    name: string
+    city: string = ""
+    constructor(email: string, name: string) {
+        this.email = email;
+        this.name = name;
+
+    }
+}
+
+const hitesh2 = new User("h@h.com", "hitesh");
+hitesh2.city = "Seoul"
