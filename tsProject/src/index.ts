1fabd0fc2670861ac36d66f60b524c53f50d43ab
ChangJin-Lee <ckdwls525@gmail.com>
2023-07-11 21:00:46 +0900

    update private public

--- tsProject/src/index.ts
+++ tsProject/src/index.ts
@@ -1,9 +1,10 @@
 // console.log("typescript is here");
 
 class User {
-    email: string
-    name: string
-    city: string = ""
+    public email: string
+    private name: string
+    // #name: string
+    readonly city: string = ""
     constructor(email: string, name: string) {
         this.email = email;
         this.name = name;
@@ -12,4 +13,16 @@ class User {
 }
 
 const hitesh2 = new User("h@h.com", "hitesh");
-hitesh2.city = "Seoul"
+// hitesh2.city = "Seoul"
+
+
+
+class User2 {
+    readonly city: string = "Seoul"
+    constructor(
+         email: string,
+         name: string,
+         private userID: string
+         ){
+    }
+}
