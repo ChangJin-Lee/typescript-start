1ac435fc6103c0f8e48c8c5bfeedbbd15236dc93
ChangJin-Lee <ckdwls525@gmail.com>
2023-07-12 20:55:03 +0900

    update getter setter

--- tsProject/src/index.ts
+++ tsProject/src/index.ts
@@ -1,6 +1,9 @@
 // console.log("typescript is here");
 
 class User {
+
+    private _courseCount = 1
+
     public email: string
     private name: string
     // #name: string
@@ -10,9 +13,30 @@ class User {
         this.name = name;
 
     }
+
+    private deleteToken(){
+        console.log("delete token");
+    }
+
+    get getAppleEmail(): string {
+        return `appele${this.email}`
+    }
+
+    get courseCount(): number {
+        return this._courseCount
+    }
+
+    set courseCount(courseNum) {
+        if(courseNum <= 1){
+            throw new Error("courseNum must be greater than 1")
+        }
+        this._courseCount = courseNum
+    }
+        
 }
 
 const hitesh2 = new User("h@h.com", "hitesh");
+//hitesh2.deleteToken();
 // hitesh2.city = "Seoul"
 
 
